48c8f74f72a4928b35fa16b1efe77dc9b007e0da
yukun <116103371+yukun1120@users.noreply.github.com>
2024-08-09 08:43:08 +0900

    piniaの永続化設定の追加

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -17,6 +17,7 @@ export default defineNuxtConfig({
     ['@pinia/nuxt', {
       autoImports: ['defineStore', 'acceptHMRUpdate'],
     }],
+    '@pinia-plugin-persistedstate/nuxt'
   ],
   vite: {
     vue: {
